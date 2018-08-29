02cb5fa67a1faf2aa14dd57ea557e80c90865e44
RealPolitiX <xrpatrick@gmail.com>
2018-08-29 19:00:31 +0200

    Updated the setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,17 @@
-import setuptools
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
 
-setuptools.setup(
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'requirements.txt'), encoding='utf-8') as f:
+    all_reqs = f.read().split('\n')
+
+install_requires = [x.strip() for x in all_reqs if 'git+' not in x]
+dependency_links = [x.strip().replace('git+', '') for x in all_reqs if x.startswith('git+')]
+
+setup(
     name="symmetrize",
     version="0.1.0",
     url="https://github.com/RealPolitiX/symmetrize",
@@ -11,9 +22,9 @@ setuptools.setup(
     description="Symmetrization and centering of 2D pattern using nonrigid point set registration",
     long_description=open('README.rst').read(),
 
-    packages=setuptools.find_packages(),
-
+    packages=find_packages(exclude=['docs', 'tests*']),
     install_requires=[],
+    dependency_links=dependency_links,
 
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
