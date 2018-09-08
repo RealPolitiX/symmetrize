f44b42633e19dd01fc593ac99c6b1788e642afd6
RealPolitiX <xrpatrick@gmail.com>
2018-09-08 17:17:55 +0200

    modified __init__.py

--- symmetrize/__init__.py
+++ symmetrize/__init__.py
@@ -1,5 +1,5 @@
 """symmetrize - Symmetrization and centering of 2D pattern using nonrigid point set registration"""
 
 __version__ = '0.1.0'
-__author__ = 'R. Patrick Xian <xrpatrick@gmail.com>'
+__author__ = 'R. Patrick Xian'
 __all__ = []
