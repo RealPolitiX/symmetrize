f998459b2b031d3ebd0a865ff4e836c41077cc49
RealPolitiX <xrpatrick@gmail.com>
2019-01-22 04:29:13 +0100

    Upgraded the package version to 0.2.3

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     
 setup(
     name="symmetrize",
-    version="0.2.2",
+    version="0.2.3",
     url="https://github.com/RealPolitiX/symmetrize",
 
     author="R. Patrick Xian",
--- symmetrize/__init__.py
+++ symmetrize/__init__.py
@@ -1,5 +1,5 @@
 """symmetrize - Symmetrization and centering of 2D pattern using nonrigid point set registration"""
 
-__version__ = '0.2.2'
+__version__ = '0.2.3'
 __author__ = 'R. Patrick Xian'
 __all__ = []
