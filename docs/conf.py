d446d40c66f48066dc2349f02795e4305d432cb2
RealPolitiX <xrpatrick@gmail.com>
2019-03-18 19:32:43 +0100

    Switched to RTD theme

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
-import guzzle_sphinx_theme
+import sphinx_rtd_theme
 
 # -- General configuration ------------------------------------------------
 
@@ -36,8 +36,7 @@ extensions = ['sphinx.ext.autodoc',
     'sphinx.ext.intersphinx',
     'sphinx.ext.mathjax',
     'sphinx.ext.viewcode',
-    'sphinx.ext.githubpages',
-    'guzzle_sphinx_theme']
+    'sphinx.ext.githubpages']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -90,8 +89,8 @@ todo_include_todos = False
 # a list of builtin themes.
 #
 # html_theme = 'alabaster'
-html_theme_path = guzzle_sphinx_theme.html_theme_path()
-html_theme = 'guzzle_sphinx_theme'
+html_theme_path = ['_themes', ]
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
